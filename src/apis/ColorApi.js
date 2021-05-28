62d5c0334620febf7fd0f059ad56bd743a34f361
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-05-28 23:05:09 +0700

    fix code

--- src/apis/ColorApi.js
+++ src/apis/ColorApi.js
@@ -5,9 +5,11 @@ const url = 'https://apicolor-demo-1.herokuapp.com/api/v1/colors'
 const ColorApi = {
     getColor: (text) => {
         if (!text) return new Promise();
-        text = text.trim().replaceAll(' ', '').toLowerCase();
+        let str = text.toLowerCase().trim().split(" ");
+        let newStr = str.join('')
+
         let params = {
-            name: text
+            name: newStr
         }
         return axiosClient.get(url, { params })
     }
